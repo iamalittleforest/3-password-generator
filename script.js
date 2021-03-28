0e172ab64cdbd7714bae49461646dc2488610226
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-03-28 11:38:07 -0700

    updated script file to define arrays and input variables

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Define character types into arrays
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "`", "{", "|", "}", "~"];
+
+// Define inputs into variables
+var passwordLength;
+var useUpperCase;
+var useLowerCase;
+var useNumber;
+var useSpecialChar;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
