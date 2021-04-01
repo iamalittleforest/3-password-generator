b076da34f39668a08b6e8a73b756da12cada935a
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-03-31 21:22:39 -0700

    defined function to generate password and integrate with html

--- script.js
+++ script.js
@@ -7,62 +7,63 @@ var specialChar = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Define function to write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Define function to generate password
+function generatePassword() {
 
-// Prompt user to input password length
-var passwordLength = window.prompt("Choose a password length between 8 and 128 characters.");
-console.log("Password Length", passwordLength);
-
-// Validate password length against requirements
-while (passwordLength < 8 || passwordLength > 128) {
-  alert("Password length must be between 8 and 128 characters.");
-  passwordLength = window.prompt("Choose a password length between 8 and 128 characters.");
+  // Prompt user to input password length
+  var passwordLength = window.prompt("Choose a password length between 8 and 128 characters.");
   console.log("Password Length", passwordLength);
-}
 
-// User confirmation to include/exclude lowercase characters
-var useLowerCase = window.confirm("Click OK to include lowercase characters.");
-console.log("Use Lowercase Characters", useLowerCase);
-
-// User confirmation to include/exclude uppercase characters
-var useUpperCase = window.confirm("Click OK to include uppercase characters.");
-console.log("Use Uppercase Characters", useUpperCase);
-
-// User confirmation to include/exclude numeric characters
-var useNumber = window.confirm("Click OK to include numeric characters.");
-console.log("Use Numeric Characters", useNumber);
-
-// User confirmation to include/exclude special characters
-var useSpecialChar = window.confirm("Click OK to include special characters.");
-console.log("Use Special Characters", useSpecialChar);
-
-// Validate character type against requirements
-while (!useLowerCase && !useUpperCase && !useNumber && !useSpecialChar) {
-  alert("At least one character type is required.");
-  useLowerCase = window.confirm("Click OK to include lowercase characters.");
-  console.log("Use Lowercase Characters", useLowerCase);
-  useUpperCase = window.confirm("Click OK to include uppercase characters.");
-  console.log("Use Uppercase Characters", useUpperCase);
-  useNumber = window.confirm("Click OK to include numeric characters.");
-  console.log("Use Numeric Characters", useNumber);
-  useSpecialChar = window.confirm("Click OK to include special characters.");
-  console.log("Use Special Characters", useSpecialChar);
+  // Validate password length against requirements
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be between 8 and 128 characters.");
+    passwordLength = window.prompt("Choose a password length between 8 and 128 characters.");
+    console.log("Password Length", passwordLength);
   }
 
-// Build the password
-var password = "";
-var selectedCharType = [];
+  // User confirmation to include/exclude lowercase characters
+  var useLowerCase = window.confirm("Click OK to include lowercase characters.");
+  console.log("Use Lowercase Characters", useLowerCase);
+
+  // User confirmation to include/exclude uppercase characters
+  var useUpperCase = window.confirm("Click OK to include uppercase characters.");
+  console.log("Use Uppercase Characters", useUpperCase);
+
+  // User confirmation to include/exclude numeric characters
+  var useNumber = window.confirm("Click OK to include numeric characters.");
+  console.log("Use Numeric Characters", useNumber);
+
+  // User confirmation to include/exclude special characters
+  var useSpecialChar = window.confirm("Click OK to include special characters.");
+  console.log("Use Special Characters", useSpecialChar);
+
+  // Validate character type against requirements
+  while (!useLowerCase && !useUpperCase && !useNumber && !useSpecialChar) {
+    alert("At least one character type is required.");
+    useLowerCase = window.confirm("Click OK to include lowercase characters.");
+    console.log("Use Lowercase Characters", useLowerCase);
+    useUpperCase = window.confirm("Click OK to include uppercase characters.");
+    console.log("Use Uppercase Characters", useUpperCase);
+    useNumber = window.confirm("Click OK to include numeric characters.");
+    console.log("Use Numeric Characters", useNumber);
+    useSpecialChar = window.confirm("Click OK to include special characters.");
+    console.log("Use Special Characters", useSpecialChar);
+    }
+
+  // Build the password
+  var password = "";
+  var selectedCharType = [];
 
   // If selected, add lowerCase array to selectedCharType array
   if (useLowerCase) {
@@ -88,4 +89,8 @@ var selectedCharType = [];
   for (var i = 0; i < passwordLength; i++) {
       password = password + selectedCharType[Math.floor(Math.random() * selectedCharType.length)];
   }
+
   console.log("Password", password);
+  return password;
+  
+}
